8eab0bfae602c63d8e928357b0f029b5d9b5d2ce
Alan-Nixon <alannixon2520@gmail.com>
2024-07-31 13:12:07 +0530

    tailwind added

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,9 +2,9 @@ import { memo } from 'react'
 
 function Home() {
     return (<>
-    
-
-    </>)
+        <img src="/bg.jpg" className="w-full h-[150px]" alt="" />
+        <p className='text-xl'>Welcome to Utility Hub</p>
+    </>)  
 }
-
+ 
 export default memo(Home)
